dadaae9380896b3d430651afa46cc7b9f716dcd2
Moztanku <mostankpl@gmail.com>
2023-10-15 14:10:10 +0200

    Refactor, add sound and music capability

--- test.cpp
+++ test.cpp
@@ -3,15 +3,17 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 
+#include <memory>
 namespace textures = Resources::textures;
+// namespace sfx = Resources::
 
-sf::SoundBuffer make_soundBuffer(const std::string& path)
-{
-    sf::SoundBuffer buffer;
-    if(!buffer.loadFromFile(path))
-        throw std::runtime_error("Failed to load sound " + path);
-    return buffer;
-}
+    std::unique_ptr<sf::Music> make_music(const std::string& path)
+    {
+        auto music = std::make_unique<sf::Music>();
+        if(!music->openFromFile(path))
+            throw std::runtime_error("Failed to load music " + path);
+        return music;
+    }
 
 int main()
 {
@@ -22,16 +24,36 @@ int main()
     // limit the framerate
     window.setFramerateLimit(60);
 
+
+
+    sf::RectangleShape shape;
+    shape.setSize({600,300});
+    shape.setTexture(&textures::skinface);
+    shape.setTextureRect({0,0, static_cast<int>(textures::skinface.getSize().x), static_cast<int>(textures::skinface.getSize().y)});
+    shape.setPosition(100,100);
+
+    sf::Sound sound{Resources::sounds::air_raid};
+
+    auto& music = *Resources::music::I_Miss_You;
+
     // create loop
     while (window.isOpen())
     {
+        if(sound.getStatus() != sf::Sound::Status::Playing){
+            sound.setPitch(1.0f + (rand() % 100) / 100.0f);
+            sound.play();
+        }
+        
         // handle events
+
+        if(Resources::music::I_Miss_You->getStatus() != sf::Music::Status::Playing){
+            Resources::music::I_Miss_You->setPitch(1.0f + (rand() % 100) / 100.0f);
+            Resources::music::I_Miss_You->play();
+        }
 
         sf::Event event;
         while (window.pollEvent(event))
         {
-            // "close requested" event: we close the window
-
             if (event.type == sf::Event::Closed)
                 window.close();
         }
@@ -41,13 +63,6 @@ int main()
         window.clear(sf::Color::Black);
 
         // draw everything here...
-
-        sf::RectangleShape shape;
-        shape.setSize({600,300});
-        shape.setTexture(&textures::skinface);
-        shape.setTextureRect({0,0, static_cast<int>(textures::skinface.getSize().x), static_cast<int>(textures::skinface.getSize().y)});
-
-        shape.setPosition(100,100);
 
         window.draw(shape);
         // end the current frame
